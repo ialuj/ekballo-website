d6e8832560e26097596d877d6d6c8e0c08dfb280
ialuj <ritsure.julai.jose@gmail.com>
2025-05-25 15:53:28 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ekballo-website/",
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
