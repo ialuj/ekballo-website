f5b235717a96b0dc2a20122444623fb143d2d8ab
ialuj <ritsure.julai.jose@gmail.com>
2025-06-12 14:32:54 +0200

    fix: add node typings and build compatibility for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   base: "/ekballo-website/",
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": resolve(__dirname, "src"),
     },
   },
 });
