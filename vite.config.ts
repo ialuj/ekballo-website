bb043ddca1399910e9473a72c99c277624ffc128
ialuj <ritsure.julai.jose@gmail.com>
2025-06-12 14:28:27 +0200

    fix: make Vite config compatible with Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   base: "/ekballo-website/",
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
